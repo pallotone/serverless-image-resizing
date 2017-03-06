a9819434423389120de3f0589ae746a48ad26548
Jason Yeomans <jyeo098@gmail.com>
2017-03-06 12:52:18 -0500

    load cover image with s3 sdk instead of request

--- ptone-image/index.js
+++ ptone-image/index.js
@@ -13,6 +13,7 @@ let BUCKET = process.env.BUCKET;
 let URL = process.env.URL;
 
 exports.handler = function (event, context) {
+    console.time('time');
     let key = event.queryStringParameters.key;
     console.log(key);
     let match = key.match(/(avatar|cover)\/(.*)\/(\d+)x(\d+)/);
@@ -45,7 +46,7 @@ exports.handler = function (event, context) {
                 }).promise()
             )
             .then(() => context.succeed({
-                    statusCode: '301',
+                    statusCode: '303',
                     headers: {'location': `${URL}/${key}`},
                     body: ''
                 })
@@ -59,7 +60,9 @@ exports.handler = function (event, context) {
             canvas = new Canvas(),
             ctx = canvas.getContext('2d');
         console.log('bout to read file');
+        console.time('readCorner');
         fs.readFile('images/ptone_corner.png', function (err, corner) {
+            console.timeEnd('readCorner');
             console.log('file redd');
             if (err) throw err;
             let cornerImg = new Image;
@@ -67,39 +70,46 @@ exports.handler = function (event, context) {
             let coverImg = new Image;
             let coverUrl = URL + '/' + originalKey;
             console.log(coverUrl);
-            request.get(coverUrl, function (err, res2, body) {
-                console.log('request done');
-                coverImg.src = body;
-                let cornerSize = 200;
-                canvas.width = 955;
-                canvas.height = 500;
-                let newWidth = (canvas.height * coverImg.width) / coverImg.height;
-                let newHeight = (coverImg.height / coverImg.width) * canvas.width;
-                let coverImgX = (canvas.width - newWidth ) * 0.5;
-                let coverImgY = (canvas.height - newHeight ) * 0.5;
-                if (newWidth < canvas.width) {
-                    coverImgX = 0;
-                    newWidth = canvas.width;
-                } else {
-                    coverImgY = 0;
-                    newHeight = canvas.height;
-                }
-                console.log(coverImgX, newWidth);
-                ctx.drawImage(coverImg, coverImgX, coverImgY, newWidth, newHeight);
-                ctx.drawImage(cornerImg, canvas.width - cornerSize, canvas.height - cornerSize, cornerSize, cornerSize);
-                S3.putObject({
-                    Body: canvas.toBuffer(),
-                    Bucket: BUCKET,
-                    ContentType: 'image/jpeg',
-                    Key: key
-                }).promise()
-                    .then(() => context.succeed({
-                        statusCode: '301',
-                        headers: {'location': `${URL}/${key}`},
-                        body: ''
-                    }))
-                    .catch((err) => context.fail(err));
-            });
+            console.time('readCover');
+            S3.getObject({Bucket: BUCKET, Key: originalKey}).promise().then(
+                (data) => {
+                    console.timeEnd('readCover');
+                    console.log('request done');
+                    coverImg.src = data.Body;
+                    let cornerSize = 200;
+                    canvas.width = 955;
+                    canvas.height = 500;
+                    let newWidth = (canvas.height * coverImg.width) / coverImg.height;
+                    let newHeight = (coverImg.height / coverImg.width) * canvas.width;
+                    let coverImgX = (canvas.width - newWidth ) * 0.5;
+                    let coverImgY = (canvas.height - newHeight ) * 0.5;
+                    if (newWidth < canvas.width) {
+                        coverImgX = 0;
+                        newWidth = canvas.width;
+                    } else {
+                        coverImgY = 0;
+                        newHeight = canvas.height;
+                    }
+                    console.log(coverImgX, newWidth);
+                    ctx.drawImage(coverImg, coverImgX, coverImgY, newWidth, newHeight);
+                    ctx.drawImage(cornerImg, canvas.width - cornerSize, canvas.height - cornerSize, cornerSize, cornerSize);
+                    S3.putObject({
+                        Body: canvas.toBuffer(),
+                        Bucket: BUCKET,
+                        ContentType: 'image/jpeg',
+                        Key: key
+                    }).promise()
+                        .then(() => {
+                            request.post(`https://graph.facebook.com/v2.8/?scrape=true&id=https://pallotone.com/t/${}`);
+                            console.timeEnd('time');
+                            context.succeed({
+                                statusCode: '303',
+                                headers: {'location': `${URL}/${key}`},
+                                body: ''
+                            });
+                        })
+                        .catch((err) => context.fail(err));
+                });
         });
     };
 
